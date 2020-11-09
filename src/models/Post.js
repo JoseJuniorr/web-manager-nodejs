d84013378880fa763ccc11a26f3a9df59694de37
José Junior <jjerrorama@gmail.com>
2020-11-09 12:28:37 -0300

    update post model e routes

---
+++ src/models/Post.js
@@ -0,0 +1,40 @@
+const { model, Schema } = require("mongoose");
+const slug = require("mongoose-url-slugs");
+
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      slug: "title",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+    },
+    images: [{ url: String, public_id: String }],
+    categoria: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Categorias",
+      },
+    ],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+PostSchema.plugin(slug("title", { update: true }));
+
+module.exports = model("Post", PostSchema);
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", function (req, res, next) {
-  res.send("rota post index");
+  res.render("posts/index", { title: " Posts" });
 });
 
 module.exports = router;
