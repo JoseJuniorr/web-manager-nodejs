10c0abef36547fb773aeb84fd0af88116e8df6c0
José Junior <jjerrorama@gmail.com>
2020-11-09 12:25:12 -0300

    adm routes e controller, administração de usuários

--- src/controllers/AdmController.js
+++ src/controllers/AdmController.js
@@ -1,10 +1,34 @@
 const AdmController = {};
 
+const User = require("../models/User");
+
 AdmController.renderDashboard = (req, res, next) => {
   res.render("adm/dashboard", { title: "Dashboard - ADM" });
 };
 
-AdmController.renderListUsers = (req, res, next) => {
-  res.render("adm/list-users", { title: "Lista de Usuários" });
+AdmController.renderListUsers = async (req, res, next) => {
+  await User.find({})
+    .then((users) => {
+      res.render("adm/list-users", {
+        title: "Lista de Usuários",
+        users: users,
+      });
+    })
+    .catch((err) => {
+      req.flash("error_msg", "Erro ao listar os usuários!");
+      res.redirect("/adm/dashboard");
+    });
+};
+
+AdmController.renderEditUser = async (req, res, next) => {
+  await User.findById(req.params.id)
+    .then((user) => {
+      res.render("adm/edit-user", { title: "Atualizar Usuário", user: user });
+    })
+    .catch((err) => {
+      console.log(err);
+      req.flash("error_msg", "Erro ao carregar o usuário!");
+      res.redirect("/adm/list-users?update=false");
+    });
 };
 module.exports = AdmController;
--- src/routes/adm.routes.js
+++ src/routes/adm.routes.js
@@ -5,10 +5,15 @@ const { isAuthenticated } = require("../middlewares/isAuthenticated");
 const {
   renderDashboard,
   renderListUsers,
+  renderEditUser,
 } = require("../controllers/AdmController");
 
 router.get("/dashboard", isAuthenticated, renderDashboard);
 
 router.get("/list-users", isAuthenticated, renderListUsers);
+
+router.get("/edit-user/:id", isAuthenticated, renderEditUser);
+
+router.put("/update-user/:id", isAuthenticated);
 
 module.exports = router;
